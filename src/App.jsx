bf1ac48ebf3d5ac81bd3f3c062c8c630f6ce4945
Biplove Kumar Singh <142970684+biplove100@users.noreply.github.com>
2026-02-04 16:07:07 +0530

    Basic Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import CoreConcept from './components/CoreConcept.jsx';
-import components from './assets/components.png';
-import config from './assets/config.png';
-import stateMgmt from './assets/state-mgmt.png';
-
 function App() {
   return (
     <div>
@@ -16,9 +11,7 @@ function App() {
       </header>
       <main>
         <h2>Time to get started!</h2>
-        <CoreConcept title="Components" description="The core UI building block - compose the user interface by combining multiple components." ImgSrc={components} />
-        <CoreConcept title="Props" description="Pass data to components." ImgSrc={config}/>
-        <CoreConcept title="State" description="Manage data that changes over time." ImgSrc={stateMgmt}/>
+        
       </main>
     </div>
   );
