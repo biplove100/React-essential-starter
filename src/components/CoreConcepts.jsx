e7e84b6f78e4b6ce7dd97ae338d5c71d755b14af
Biplove Singh <biplovesinghrajpute.bsr1@gmail.com>
2026-02-13 09:51:41 +0530

    This is the final Codebase

---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,20 @@
+import CoreConcept from "./CoreConcept"
+import { CORE_CONCEPTS } from "../data.js";
+
+export default function CoreConcepts() {
+    return(
+        <section id='core-concepts'>
+                  <h2>Core Concepts</h2>
+                  <ul>
+                    {/* OUTPUTTING LIST DATA DYNAMICALLY USING map function in Array */}
+                    {CORE_CONCEPTS.map((conceptItem) => (
+                      <CoreConcept key={conceptItem.title} {...conceptItem} />
+                    ))}
+                    {/* <CoreConcept {...CORE_CONCEPTS[0]} />
+                    <CoreConcept {...CORE_CONCEPTS[1]} />
+                    <CoreConcept {...CORE_CONCEPTS[2]} />
+                    <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+                  </ul>
+                </section>
+    )
+}
---
+++ src/components/Examples.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import { EXAMPLES } from '../data.js';
+import TabButton from './TabButton.jsx';
+import Section from './Section.jsx';
+
+export default function Examples() {
+    const [selectedTopic, setSelectedTopic] = useState();
+
+  let tabContent = <p>Please select a Topic</p>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id='tab-content'>
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    )
+  }
+
+  function handleSelect(selectedButton) {
+    //Selected Button can be Components, JSX, Props, State    
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
+
+    return(
+        <Section title='Examples' id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {/* CONDITIONAL RENDERING HERE: */}
+          {/* USING TERNARY OPERATOR */}
+          {/* {!selectedTopic ? <p>Please select a Topic</p> :
+            <div id='tab-content'>
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          } */}
+
+          {/* USING && WITH THE TRUTH VALUE RENDERS THE VALUE */}
+          {/* {!selectedTopic && <p>Please select a Topic</p>}
+          {selectedTopic && <div id='tab-content'>
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          } */}
+
+          {/* CONDITIONAL RENDERING USING THE VARIABLE */}
+          {tabContent}
+        </Section>
+    );
+}
